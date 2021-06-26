9536cec774a95e353abc0ac9b70e1d7049f6bb86
ihassab202 <i.hasab@robone.net>
2021-06-26 23:07:54 +0200

    Adjusted correlation effeciency

--- autocor.py
+++ autocor.py
@@ -12,6 +12,7 @@ def get_resultant(X,Y,Z):
 def get_mean(arr):
     """This function uses numpy's mean funtion to calculate the mean of values in an array"""
     return np.mean(arr)
+
 def get_sd(arr):
     """This function uses numpy's std funtion to calculate the standard deviation of values in an array"""
     return np.std(arr)
@@ -20,22 +21,25 @@ def sumnomeq(y,k):
     """This function calculates the sum of all iterations of the nominaor in the autocorrelation formula"""
     nomeq = []
     end = len(y) - k
+    mean = get_mean(y)
     for i in range(0,end):
-        nomeq.append((y[i] - get_mean(y)) * ( y[i + k] - get_mean(y)))
-    return sum(nomeq[i] for i in range(0, end))
+        nomeq.append((y[i] - mean) * ( y[i + k] - mean))
+    return np.sum(nomeq)
 def sumdenomeq(y):
     """This function calculates the sum of all iterations of the denominaor in the autocorrelation formula"""
     denomeq = []
+    mean = get_mean(y)
     end = len(y) - 1
     for i in range(0,end):
-        denomeq.append((y[i] - get_mean(y))**2)
-    return sum(denomeq[i] for i in range(0, end))
+        denomeq.append((y[i] - mean)**2)
+    return np.sum(denomeq)
 def autocor(y):
     """Autocorrelation of array y"""
     autocorarr = []
     counter = 0
+    denom = sumdenomeq(y)
     for i in range (len(y)-1):
-        autocorarr.append((sumnomeq(y,i)/sumdenomeq(y)))
+        autocorarr.append((sumnomeq(y,i)/denom))
         counter += 1
         print(f"Progress: {counter*100/len(y)}%")
     return autocorarr
@@ -57,10 +61,14 @@ if __name__ == '__main__':
     """Finds the standard deviation of resultant Magnetometer readings"""
     Magn_sd = get_sd(Magn_resultant)
 
+    autocorr_magn = autocor(MagnX)#np.load("autocorrelation_magn.npy",allow_pickle=True) 
+    import plot
+    plot.plot_2d(np.arange(0,len(MagnX),1),'record number',[np.append(autocorr_magn,autocorr_magn[-1])],'Autocorrelation',[MagnX],"Magn")
+
     plt.plot(np.arange(0,len(MagnX),1),Magn_resultant,label='MagnResultant')
     plt.plot(np.arange(0,len(MagnX),1),[Magn_mean]*len(MagnX),label='Mean')
     plt.plot(np.arange(0,len(MagnX),1),[Magn_sd]*len(MagnX),label='Numpy Standard Deviation')
-    plt.plot(np.arange(0,len(MagnX),1),autocor(Magn_resultant),label='Autocorrelation')
+    plt.plot(np.arange(0,len(MagnX),1),autocorr_magn+[autocorr_magn[-1]],label='Autocorrelation')
    
     """plots resultant magnetometer values, the mean, and the standard deviation"""
     plt.legend()
