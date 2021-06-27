2ab6dcf00f21a54325c6989805816bb6631c123b
ihassab202 <i.hasab@robone.net>
2021-06-27 22:46:14 +0200

    fixed error

--- experiment-2.py
+++ experiment-2.py
@@ -1,8 +1,6 @@
 """ Importing the libraries and the modules """
 from datetime import datetime,timedelta
-#import API
 import magn
-#import map
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -45,14 +43,8 @@ gyroY_filtered = gyro_filtered_matrix[:,1]
 gyroZ_filtered = gyro_filtered_matrix[:,2]
 
 magn_filtered_resultant = magn.get_resultant(magn_filtered_matrix[:,0],magn_filtered_matrix[:,1],magn_filtered_matrix[:,2])
-#magnX_filtered = magn_filtered_matrix[:,0]
-#magnY_filtered = magn_filtered_matrix[:,1]
-#magnZ_filtered = magn_filtered_matrix[:,2]
 
 acc_filtered_resultant = magn.get_resultant(acc_filtered_matrix[:,0],acc_filtered_matrix[:,1],acc_filtered_matrix[:,2])
-#accX_filtered = acc_filtered_matrix[:,0]
-#accY_filtered = acc_filtered_matrix[:,1]
-#accZ_filtered = acc_filtered_matrix[:,2]
 
 ''' 3- Calculating the resultant magnitude, the standard deviation, the mean and the auto corelation of the points
 for the magnetic field, and the acceleration, and the angular rate'''
@@ -69,55 +61,50 @@ gyro_mean_mean = magn.get_mean_3(gyroX_mean, gyroY_mean, gyroZ_mean,)
 
 magn_sd = magn.get_sd(magn_filtered_resultant)
 magn_mean = magn.get_mean(magn_filtered_resultant)
-#magn_autocorrelation = magn.autocor(magn_filtered_resultant)
+magn_autocorrelation = magn.autocor(magn_filtered_resultant)
 
 acc_sd = magn.get_sd(acc_filtered_resultant)
 acc_mean = magn.get_mean(acc_filtered_resultant)
-#acc_autocorrelation = magn.autocor(acc_filtered_resultant)
+acc_autocorrelation = magn.autocor(acc_filtered_resultant)
 
 '''4- Plotting the graphs '''
-
 #Adjusting the format of the time for the x-axis by flooring the seconds to remove the seconds fraction
 for i in range(len(time)):
     time[i] = time[i].split('.')[0]
-"""
 plt.title("Magnometer Readings")
-plt.xlabel("Time")
+plt.xlabel("Reading number")
 plt.ylabel("Magnetic Intensity/µT")
-plt.plot(time,magnX,label='MagnX')
-plt.plot(time,magnY,label='MagnY')
-plt.plot(time,magnZ,label='MagnZ')
+plt.plot(magnX,label='MagnX')
+plt.plot(magnY,label='MagnY')
+plt.plot(magnZ,label='MagnZ')
 plt.legend()
 plt.show()
-"""
 
 #plots accelerometer data
-"""
 plt.title("Accelerometer Data")
-plt.xlabel("Time")
+plt.xlabel("Reading number")
 plt.ylabel("Acceleration/g")
-plt.plot(time,acc_filtered_resultant,label='AccResultant')
-plt.plot(time,[acc_mean]*len(accX),label='Mean')
-plt.plot(time,[acc_sd]*len(accX),label='Standard Deviation')
+plt.plot(acc_filtered_resultant,label='AccResultant')
+plt.plot([acc_mean]*len(accX),label='Mean')
+plt.plot([acc_sd]*len(accX),label='Standard Deviation')
 plt.legend()
 plt.show()
 
 #plots magnetometer data
 plt.title("Magnetometer Data")
-plt.xlabel("Time")
 plt.ylabel("Magnetic Intensity/µT")
-plt.plot(time,magn_filtered_resultant,label='MagnResultant')
-plt.plot(time,[magn_mean]*len(magnX),label='Mean')
-plt.plot(time,[magn_sd]*len(magnX),label='Standard Deviation')
+plt.xlabel("Reading number")
+plt.plot(magn_filtered_resultant,label='MagnResultant')
+plt.plot([magn_mean]*len(magnX),label='Mean')
+plt.plot([magn_sd]*len(magnX),label='Standard Deviation')
 plt.legend()
 plt.show()
 
-"""
 magn_acc_cor = magn.cor(magn_filtered_resultant, acc_filtered_resultant)
 
 #plots correlation data
 plt.title("Correlation between Acceleration and Magnetic Intensity")
-plt.xlabel("Time")
-plt.plot(time,magn_acc_cor,label='Correlation')
+plt.xlabel("Reading number")
+plt.plot(magn_acc_cor,label='Correlation')
 plt.legend()
 plt.show()
